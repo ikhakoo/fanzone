066de1fcac7a9d0b6a3a3c23472856564c27e93a
ikhakoo <ikhakoo@gmail.com>
2015-05-07 11:52:38 -0400

    add video id to comments table to map video comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507155041) do
+ActiveRecord::Schema.define(version: 20150507155159) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150507155041) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "video_id"
   end
 
   create_table "relationships", force: :cascade do |t|
