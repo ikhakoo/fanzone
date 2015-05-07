56d3c1cb53c38d3b7ff9e68dad8cb53aee00deef
ikhakoo <ikhakoo@gmail.com>
2015-05-07 11:51:11 -0400

    add user id to videos table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507153737) do
+ActiveRecord::Schema.define(version: 20150507155041) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150507153737) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
