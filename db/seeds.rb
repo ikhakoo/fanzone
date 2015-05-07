dd7e4ba8e3d41042fa13c350c082810a640a3dbc
ikhakoo <ikhakoo@gmail.com>
2015-05-07 15:35:28 -0400

    seeding and updating layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@faker-game.com"
+  password = "password"
+  slug = Faker::Internet.slug
+  User.create!(name: name,
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              slug: slug
+             )
+  print "|"
+end
+
+# Microposts
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.tweets.create!(content: content) }
+  print "|"
+end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow!(followed) }
+followers.each { |follower| follower.follow!(user) }
