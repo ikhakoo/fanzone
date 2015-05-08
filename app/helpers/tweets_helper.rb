163d91699ebf57f65b7c28bca1b102f93b91bbd1
ikhakoo <ikhakoo@gmail.com>
2015-05-08 10:58:09 -0400

    working on searching hashtags

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -4,6 +4,14 @@ module TweetsHelper
   	tags.split(",").map{|tag| link_to tag.strip, tag_path(tag.strip) }.join(", ") 
 	end
 
+	# def tag_cloud(tags, classes)
+	#   max = tags.sort_by(&:count).last
+	#   tags.each do |tag|
+	#     index = tag.count.to_f / max.count * (classes.size-1)
+	#     yield(tag, classes[index.round])
+	#   end
+ #  end
+
   def wrap(content)
     sanitize(raw(content.split.map { |s| wrap_long_string(s) }.join(' ')))
   end
