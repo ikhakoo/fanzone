1dcc1882785a2d706eb44baa76e900ee02fe1880
ikhakoo <ikhakoo@gmail.com>
2015-05-23 18:41:33 -0400

    added pagination for search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
 
  	def index
-	  @entries = Entry.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%")
-	  @videos  = Video.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%")
-	  @tweets  = Tweet.where("LOWER(content) LIKE LOWER(?)", "%#{params[:search]}%")
+	  @entries = Entry.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%").paginate(page: params[:page])
+	  @videos  = Video.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%").paginate(page: params[:page])
+	  @tweets  = Tweet.where("LOWER(content) LIKE LOWER(?)", "%#{params[:search]}%").paginate(page: params[:page])
 	end
 
 end
