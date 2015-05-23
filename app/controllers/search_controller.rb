6bdfde54f4b1c05d3cfdc57d1653e567ea00b9a9
ikhakoo <ikhakoo@gmail.com>
2015-05-23 18:38:19 -0400

    added search results page and fixed controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,9 @@
 class SearchController < ApplicationController
 
  	def index
-    @entries 	= Entry.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%")
-    @videos 	= Video.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%")
-    @tweets 	= Tweet.where("LOWER(content) LIKE LOWER(?)", "%#{params[:search]}%")
+	  @entries = Entry.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%")
+	  @videos  = Video.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%")
+	  @tweets  = Tweet.where("LOWER(content) LIKE LOWER(?)", "%#{params[:search]}%")
 	end
 
 end
